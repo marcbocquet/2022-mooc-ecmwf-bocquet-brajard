c684397dfc6ba4ac703c7c10d752efa8bcbd6be0
Julien Brajard <julaja@fram.ad.nersc.no>
2022-12-21 14:38:30 +0100

    finish first version part 2 and 3

--- Model/neuralnet.py
+++ Model/neuralnet.py
@@ -1,6 +1,8 @@
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Layer
-
+import pickle
+from tensorflow.keras.models import load_model
+from .lorenz96 import l96
 
 class Periodic1DPadding(Layer):
     """Add a periodic padding to the output of a layer
@@ -34,3 +36,21 @@ class Periodic1DPadding(Layer):
         leftborder = inputs[:, -vleft:, :]
         rigthborder = inputs[:, :vright, :]
         return K.concatenate([leftborder, inputs, rigthborder], axis=-2)
+    
+    
+
+class l96hybrid(l96):
+    def __init__(self, saved_model, **kwargs):
+        super().__init__(**kwargs)
+        self.saved_model = saved_model
+        fnn = f'{self.saved_model}_nn'
+        scalerx =  f'{self.saved_model}_scalerx'
+        scalery =  f'{self.saved_model}_scalery'
+        self.scaler_x = pickle.load(open(scalerx, 'rb'))
+        self.scaler_y = pickle.load(open(scalery, 'rb'))
+        self.nn = load_model(fnn)
+    def __call__(self, q):
+        dq_phi = super().__call__(q)
+        #dq_nn =  self.scaler_y.inverse_transform(self.nn.predict(self.scaler_x.transform(q), verbose=0).squeeze())
+        dq_nn =  self.scaler_y.inverse_transform(self.nn(self.scaler_x.transform(q)).numpy().squeeze())
+        return dq_phi + dq_nn
--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ from sklearn.metrics import r2_score, mean_squared_error
 import seaborn as sns
 import tensorflow as tf
 from tqdm.notebook import tqdm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import pyplot as plt
 
 def scatter_plot(x,y,ax=None):
     ax = sns.regplot(x=x, y=y, ax=ax)
@@ -29,3 +31,42 @@ class tqdm_callback(tf.keras.callbacks.Callback):
             self.metrics[name]  = logs.get(name, self.metrics[name])
         self.epoch_bar.set_postfix(mse=self.metrics['loss'], val_mse=self.metrics['val_loss'], refresh=False)
         self.epoch_bar.update()
+
+        
+        
+#For memory, how to plot the difference (not used):
+def plot():
+    fig, ax = plt.subplots(nrows=2, sharex= True, figsize=(18, 12))
+    plt.grid(False)
+    im = ax[0].imshow(xhyb[2].T, 
+               aspect = 'auto',
+               origin = 'lower',
+               interpolation = 'spline36',
+               cmap = sns.diverging_palette(240, 60, as_cmap=True),
+               extent = [0, dt*xhyb[2].shape[0], 0, Nx],
+               vmin = -10,
+               vmax = 15)
+    divider = make_axes_locatable(ax[0])
+    cax = divider.append_axes('right', size='2%', pad=0.2)
+
+    fig.colorbar(im, cax=cax)
+    ax[0].set_ylabel('Hybrid model variables')
+
+
+    im = ax[1].imshow(xhyb[2].T-Xtest[2].T, 
+               aspect = 'auto',
+               origin = 'lower',
+               interpolation = 'spline36',
+               cmap = sns.color_palette("vlag", as_cmap=True),
+               extent = [0, dt*xhyb[2].shape[0], 0, Nx],
+               vmin = -10,
+               vmax = 10)
+    divider = make_axes_locatable(ax[1])
+    cax = divider.append_axes('right', size='2%', pad=0.2)
+
+    fig.colorbar(im, cax=cax)
+    ax[1].set_ylabel('Hybrid - True')
+
+
+    ax[1].set_xlabel('Time (MTU)')
+    #ax[0].set_tick_params(direction='out', left=True, bottom=True)
